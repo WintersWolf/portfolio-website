d88b811b0b9de0fa0f380293bcdf8f0d04bf8e3c
Daniel Winters <nosuchlogic@gmail.com>
2022-04-02 18:09:27 +1000

    Introduce more styling to index.
    Introduce more styling elements to index page.
    
    Clean up CSS and introduce more icons and styling to closer lay the groundwork for the envisioned style.

--- js/accordion.js
+++ js/accordion.js
@@ -1,21 +1,26 @@
 var acc = document.getElementsByClassName("explorerButton");
 var i;
 var x = document.getElementById("demo");
+changeIcon();
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-
-    if (x.innerHTML === "navigate_next") {
-      x.innerHTML = "expand_more";
-    } else {
-      x.innerHTML = "navigate_next";
-    }
+    changeIcon();
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
+    
   });
+}
+
+function changeIcon(){
+  if (x.innerHTML == "navigate_next") {
+    x.innerHTML = "expand_more";
+  } else {
+    x.innerHTML = "navigate_next";
+  }
 }
